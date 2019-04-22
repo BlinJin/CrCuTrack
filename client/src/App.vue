100e255829c8ba99c4f8736b7f1284987d3e7136
Uladzimir Akulenka <uladzimir.akulenka@labinvent.com>
2019-04-22 23:14:03 +0300

    chore(App) config stylus preprocessor

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,13 +4,3 @@
     </div>
 </template>
 
-<style scoped>
-    .full-width {
-        width: 100%;
-    }
-    .center-content {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-</style>
