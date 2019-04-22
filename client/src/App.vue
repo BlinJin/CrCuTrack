000d0f56d99a43090f2f2347c2386d62d11b8133
Uladzimir Akulenka <uladzimir.akulenka@labinvent.com>
2019-04-22 23:00:44 +0300

    chore(App) add same styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,3 +3,14 @@
         <h1>Hello World Vue!:):)</h1>
     </div>
 </template>
+
+<style scoped>
+    .full-width {
+        width: 100%;
+    }
+    .center-content {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
