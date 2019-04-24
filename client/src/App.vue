cf0f9d15ff39578037ee6b2e63cfc615251fd34e
BlinJin <imax13@mail.ru>
2019-04-25 00:04:40 +0300

    chore(axios) set up axios lib

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <hello-component name="World" />
+    <currency-dashboard />
   </div>
 </template>
 
 <script>
 import HelloComponent from './components/HelloComponent.vue'
+import CurrencyDashboard from './components/CurrencyDashboard.vue'
 export default {
   components: {
-    HelloComponent
+    HelloComponent,
+    CurrencyDashboard
   }
 }
 </script>
--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '../assets/app.css'
+import Axios from 'axios';
+
+Axios.defaults.baseURL = process.env.API_ENDPOINT;
 
 new Vue({
   el: '#app',
