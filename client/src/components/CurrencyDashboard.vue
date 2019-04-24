e254eb7541b7f5c7c176928a9e232a2e2cf093d7
BlinJin <imax13@mail.ru>
2019-04-25 00:04:13 +0300

    chore(CurrencyDashboard) add currency dashboard component

---
+++ client/src/components/CurrencyDashboard.vue
@@ -0,0 +1,30 @@
+<template>
+    <p class="homeText">
+        WELCOME TO CURRENCY APPLICATION
+    </p>
+</template>
+<script>
+    import CurrencyService from '../api-services/currency.service';
+
+    export default {
+        name: "CurrencyDashboard",
+        created() {
+            CurrencyService.getAll().then((response) => {
+                console.log(response.data);
+            }).catch((error) => {
+                console.log(error.response.data);
+            });
+        }
+    }
+</script>
+
+<style scoped>
+    .homeText{
+        font-size: 35px;
+        color: red;
+        text-align: center;
+        position: relative;
+        top:30px;
+        text-shadow: 2px 2px 2px gray;
+    }
+</style>
